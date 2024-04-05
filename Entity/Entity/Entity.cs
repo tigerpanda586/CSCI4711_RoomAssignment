a978212b8a2acd91a7b98a8981efcaaf024fb30a
Jessica Brown <TreePerson586@yahoo.com>
2024-04-05 10:48:09 -0400

    code
    code

---
+++ Entity/Entity/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Entity
+{
+    public class RequestList
+    {
+        public Request[] requests;
+        public Request[] getReq()
+        {
+            return requests;
+        }
+    }
+
+    public class RoomList 
+    {
+        public Room[] rooms;
+        public Room[] getRooms()
+        {
+            return rooms;
+        }
+    }
+
+
+
+}
