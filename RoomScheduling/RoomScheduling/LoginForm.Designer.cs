5b61854ab7042bdc0ec836bfa4847bb698f6336a
Jessica Brown <TreePerson586@yahoo.com>
2024-04-24 17:19:12 -0400

    UI works
    it is some uis

--- RoomScheduling/RoomScheduling/LoginForm.Designer.cs
+++ RoomScheduling/RoomScheduling/LoginForm.Designer.cs
@@ -90,6 +90,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox2.Location = new System.Drawing.Point(313, 217);
             this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(178, 26);
             this.textBox2.TabIndex = 4;
             // 
